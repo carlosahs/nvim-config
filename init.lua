da959f498c5bb240ddde902529583a3329f934c0
carlosahs <tacahs12@gmail.com>
2021-09-19 20:53:33 -0500

    Add txt indent file

--- init.lua
+++ init.lua
@@ -161,7 +161,7 @@ require('telescope').setup {
     },
   },
 }
--- Telescope shortcuts
+-- Telescope keybindings
 vim.api.nvim_set_keymap('n', '<leader><space>', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>sf', [[<cmd>lua require('telescope.builtin').find_files({previewer = false})<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>sb', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]], { noremap = true, silent = true })
