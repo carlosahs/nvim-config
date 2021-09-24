497eff8dac4f3e90a98ac0bb25d611a3385d6cd0
carlosahs <tacahs12@gmail.com>
2021-09-24 16:12:44 -0500

    Add Java template for kick start

---
+++ templates/kick-start.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+class KickStart
+{
+    public static void main(String[] args)
+    {
+        try (Scanner in = new Scanner(System.in))
+        {
+            int cases = in.nextInt();
+            for (int t = 1; t <= cases; t++)
+            {
+                System.out.println("Case #" + t + ": ");
+            }
+        }
+    }
+}
