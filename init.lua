1ad9749ce4d9f80ba811b4494a1f847a1e37ee0b
carlosahs <tacahs12@gmail.com>
2021-09-26 10:54:25 -0500

    Switch rome lsp for tsserver lsp

--- init.lua
+++ init.lua
@@ -257,7 +257,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- Enable the following language servers
-local servers = { 'clangd', 'rust_analyzer', 'pyright', 'texlab', 'solargraph', 'rome' }
+local servers = { 'clangd', 'rust_analyzer', 'pyright', 'texlab', 'solargraph', 'tsserver' }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
