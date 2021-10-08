93f5837df7f2d9c125532e8b71c18269de35d5aa
carlosahs <tacahs12@gmail.com>
2021-10-08 10:32:32 -0500

    Update formatting

--- templates/cp.cpp
+++ templates/cp.cpp
@@ -2,12 +2,10 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     int cases;
     cin >> cases;
-    for (int t = 1; t <= cases; t++)
-    {
+    for (int t = 1; t <= cases; t++) {
 
     }
     return 0;
