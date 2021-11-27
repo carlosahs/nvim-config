240dbf5e4bf7dc4304c1ec836ce9e9db7f2f636e
carlosahs <tacahs12@gmail.com>
2021-11-27 15:28:41 -0600

    Add keymaps module

---
+++ lua/keymaps.lua
@@ -0,0 +1,19 @@
+-- easy keymaps
+function noremap(mode, lhs, rhs)
+  vim.api.nvim_set_keymap(mode, lhs, rhs, { noremap = true, silent = true })
+end
+
+function nnoremap(lhs, rhs) noremap('n', lhs, rhs) end
+
+-- window navigation
+nnoremap("<Space>h", "<C-W>h")
+nnoremap("<Space>j", "<C-W>j")
+nnoremap("<Space>k", "<C-W>k")
+nnoremap("<Space>l", "<C-W>l")
+
+-- source lua file
+nnoremap("<C-r>", ":luafile %<CR>")
+
+-- file editing
+nnoremap("<Space>w", ":w<CR>")
+nnoremap("<Space>q", ":q<CR>")
