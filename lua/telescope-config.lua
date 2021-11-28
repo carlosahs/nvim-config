6540e580ce2eb584225360de91711f046f45a080
carlosahs <tacahs12@gmail.com>
2021-11-28 15:14:43 -0600

    Configure telescope

--- lua/telescope-config.lua
+++ lua/telescope-config.lua
@@ -7,3 +7,15 @@ telescope.setup {
     }
   }
 }
+
+local nnoremap_opts = require("utils.keymaps").nnoremap_opts
+
+nnoremap_opts("<Space>P", 
+              "<cmd>lua require('telescope.builtin').find_files()<CR>",
+              { noremap = true })
+nnoremap_opts("<Space>F", 
+              "<cmd>lua require('telescope.builtin').live_grep()<CR>",
+              { noremap = true })
+nnoremap_opts("<Space>B", 
+              "<cmd>lua require('telescope.builtin').buffers()<CR>",
+              { noremap = true })
