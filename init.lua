d23744b07cc62be46058a22d7e4679d1958257b8
carlosahs <tacahs12@gmail.com>
2021-11-28 15:30:32 -0600

    Add LSP support

--- init.lua
+++ init.lua
@@ -31,4 +31,5 @@ return require('packer').startup(function()
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
+  use 'neovim/nvim-lspconfig'
 end)
