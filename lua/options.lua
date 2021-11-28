5a59d6912aacb03446d376116c6b92eee54943af
carlosahs <tacahs12@gmail.com>
2021-11-28 15:15:12 -0600

    Add indentation options

--- lua/options.lua
+++ lua/options.lua
@@ -4,4 +4,11 @@ vim.wo.number = true
 -- more natural splits
 vim.o.splitbelow = true
 vim.o.splitright = true
+
+-- relative number
 vim.o.relativenumber = true
+
+-- indentation config
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
