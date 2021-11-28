c631e212acd207eafd91c2d573c67c8d56c79008
carlosahs <tacahs12@gmail.com>
2021-11-28 14:58:00 -0600

    Add noremaps with options

--- lua/utils/keymaps.lua
+++ lua/utils/keymaps.lua
@@ -1,11 +1,24 @@
 local KeyMaps = {}
 
--- easy keymaps
-function noremap(mode, lhs, rhs)
-  vim.api.nvim_set_keymap(mode, lhs, rhs, { noremap = true, silent = true })
+-- keymaps with opts
+function KeyMaps.noremap_opts(mode, lhs, rhs, opts)
+  vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
 end
 
-function KeyMaps.nnoremap(lhs, rhs) noremap('n', lhs, rhs) end
-function KeyMaps.tnoremap(lhs, rhs) noremap('t', lhs, rhs) end
+function KeyMaps.nnoremap_opts(lhs, rhs, opts)
+  KeyMaps.noremap_opts('n', lhs, rhs, opts)
+end
+
+function KeyMaps.tnoremap_opts(lhs, rhs, opts)
+  KeyMaps.noremap_opts('t', lhs, rhs, opts)
+end
+
+-- keymaps with default opts
+function KeyMaps.noremap(mode, lhs, rhs)
+  KeyMaps.noremap_opts(mode, lhs, rhs, { noremap = true, silent = true })
+end
+
+function KeyMaps.nnoremap(lhs, rhs) KeyMaps.noremap('n', lhs, rhs) end
+function KeyMaps.tnoremap(lhs, rhs) KeyMaps.noremap('t', lhs, rhs) end
 
 return KeyMaps
