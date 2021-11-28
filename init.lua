26866d1707e36018acd8a0dc0944cff8c5bc6f1e
carlosahs <tacahs12@gmail.com>
2021-11-28 15:13:53 -0600

    Setup treesitter

--- init.lua
+++ init.lua
@@ -1,2 +1,33 @@
+require('coc-config')
+require('colors')
+require('commands')
 require('keymaps')
 require('options')
+require('telescope-config')
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = "maintained",
+  context_commentstring = {
+    enable = true
+  },
+  highlight = {
+    enable = true
+  },
+  indent = {
+    enable = true
+  }
+}
+
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'folke/tokyonight.nvim'
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+end)
