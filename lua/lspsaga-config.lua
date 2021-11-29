8179535d871b3ab1b31c05a96bebc6ae6c5ef396
carlosahs <tacahs12@gmail.com>
2021-11-29 06:33:16 -0600

    Add lsp config files

---
+++ lua/lspsaga-config.lua
@@ -0,0 +1,17 @@
+local saga = require('lspsaga')
+
+saga.init_lsp_saga()
+
+local KeyMaps = require('utils.keymaps')
+local nnoremap = KeyMaps.nnoremap
+
+-- lsp finder
+nnoremap('gh', ':Lspsaga lsp_finder<CR>')
+
+-- hover doc
+nnoremap('K', ':Lspsaga hover_doc<CR>')
+nnoremap('<C-F>', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>")
+nnoremap('<C-B>', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>")
+
+-- rename
+nnoremap('<Space>rn', ':Lspsaga rename<CR>')
