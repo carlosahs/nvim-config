35728ea3d4d6bc8c0b7650df5f38f79c3fd18188
carlosahs <tacahs12@gmail.com>
2021-12-21 11:57:09 -0600

    Modify competitive programming template

--- templates/cp.cpp
+++ templates/cp.cpp
@@ -2,17 +2,10 @@
 
 using namespace std;
 
-typedef long long ll;
-
 int main() {
     // faster input and output
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int cases;
-    cin >> cases;
-    for (int t = 1; t <= cases; t++) {
-
-    }
     return 0;
 }
