fcf3de1e994f4d00747f083cc2f7219527fc2927
carlosahs <tacahs12@gmail.com>
2021-12-27 07:53:49 -0600

    Update competitive programming template

--- templates/cp.cpp
+++ templates/cp.cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+typedef unsigned int u32;
+typedef unsigned long long u64;
+
+typedef int i32;
+typedef long long i64;
+
 int main() {
     // faster input and output
     ios::sync_with_stdio(0);
